01990ee0fb0097beb239c1f12379a3bd17354996
interglobalmedia <interglobalmedia@gmail.com>
2016-10-18 18:06:25 -0400

    Fixed workflow issue. Had to remove cssnext and precss. They were big trouble for my javscript code.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,8 +1,6 @@
 var gulp = require('gulp'),
     postcss = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
-    cssnext = require('cssnext'),
-    precss = require('precss'),
     cssvars = require('postcss-simple-vars'),
     nested = require('postcss-nested'),
     cssImport = require('postcss-import'),
@@ -11,7 +9,7 @@ var gulp = require('gulp'),
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer, precss, cssnext]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
     .on('error', function(errorInfo) {
       console.log(errorInfo.toString());
       this.emit('end');
