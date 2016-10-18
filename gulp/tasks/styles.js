f08c747201650d15c17b4733b3de86fbf4aa6d0d
interglobalmedia <interglobalmedia@gmail.com>
2016-10-18 07:58:18 -0400

    Adding gulp/tasks/. styles.js and watch.js.

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cssnext = require('cssnext'),
+    precss = require('precss'),
+    cssvars = require('postcss-simple-vars'),
+    nested = require('postcss-nested'),
+    cssImport = require('postcss-import'),
+    mixins = require('postcss-mixins');
+
+gulp.task('styles', function() {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([mixins, cssImport, cssvars, nested, autoprefixer, precss, cssnext]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
